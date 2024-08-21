0a4c697a8b2d479899bcb26baace6a051d7f9d98
JK <jk@chequer.io>
2024-08-21 21:42:01 +0900

    성능테스트: DAC 부하테스트 스크립트 리팩토링 (#401)
    ## Description
    - TypeScript 로 작성된 DAC 부하테스트 스크립트를 리팩토링하였습니다. IntelliJ 를 기준으로,
    TypeScript 문법 관련한 경고가 나타나지 않게 코드를 수정하거나, `@ts-ignore`, `// noinspection
    blah` 등 코멘트를 추가하였습니다.
    - Grafana-K6 부하 생성 스크립트는, 실행 도중 별도의 console logging 을 지원하지 않는 편입니다.
    `console.log()` 를 이용해, 디버깅 메시지를 남길 수는 있지만, Metric object 에서 실행 도중의 값을
    확인하거나, 출력하는 인터페이스를 제공하지 않습니다.
    - K6 Lifecycle 에 맞추어 코드를 정리하고, 사용되지 않는 코드를 삭제하였습니다.
    - 과부하 발생시, JavaScript 에서 Exception 이 발생하게 됩니다. 이 경우, queryFailures count
    metric 을 증가시킵니다. queryCount metric 은 성공/실패와 무관하게, count 를 증가시킵니다.
    - TODO(JK)
    - `Counter()` metric 의 이름을 바꾸는 것을 고려해 봅니다. 이때, Grafana dashboard 를 같이
    수정해야 합니다.
       - Grafana dashboard 에서 queryFailures 를 시각화하지 않고 있습니다. 이를 개선해 봅니다.
    
    ## Related tickets & links
    - N/A
    
    ## Added/updated tests?
    - [ ] Yes
    - [x] No, and this is why: `infra 구성과 문서 업데이트`
    - [ ] I need help with writing tests
    
    ## Additional notes
    -
    
    Co-authored-by: JK <jk@querypie.com>

--- k6-run/scripts/dac.ts
+++ k6-run/scripts/dac.ts
@@ -1,85 +1,74 @@
-import sql from "k6/x/sql";
-import { check } from "k6"
-import { sleep } from "k6"
-import { Counter,Trend,Rate } from "k6/metrics";
+// @ts-ignore
+import sql from "k6/x/sql"
+// @ts-ignore
+import {check, sleep} from "k6"
+// @ts-ignore
+import {Counter, Trend} from "k6/metrics"
+
+
+// K6 Test lifecycle 에 대해 이 링크를 참고하세요.
+// https://k6.io/docs/using-k6/test-lifecycle/
+
+// 1. init code (call once per VU)
+
+const queryCount = new Counter("query_per_second");
+const queryFailures = new Counter("failed_queries");
+const queryDuration = new Trend("query_duration", true /* isTime */);
 
 // The second argument is a MySQL connection string, e.g.
 // myuser:mypass@tcp(127.0.0.1:3306)/mydb
-
-// [K6 lifecycle stages]
-
-// 1. init code
-// Code in the init context prepares the script, loading files, importing modules, and defining the test lifecycle functions. Required.
-// Purpose
-// Load local files, import modules, declare lifecycle functions (Open JSON file, Import module; Called Once per VU*)
-// * In cloud scripts, init code might be called more often.
-
 const db = sql.open(
     "mysql",
     "pr0521a2f9-fbc6-4e56-9bc6-378cee5f3040:9dd84c99c84a4a99@tcp(host.docker.internal:40001)/employees"
     // "pr0521a2f9-fbc6-4e56-9bc6-378cee5f3040:9dd84c99c84a4a99@tcp(172.31.13.109:40001)/employees"
 );
 
-const queryCounter = new Counter("query_per_second");
-const failedQueryCounter = new Counter("failed_queries");
-const queryDurationTrend = new Trend("query_duration", true);
-
-const query =
-    `UPDATE salaries SET salary = 0 WHERE emp_no IN (
-  SELECT a.emp_no from (
-      SELECT * FROM salaries WHERE salary != 0 LIMIT 10
-    ) a
-)`
-const query2 = `SELECT * FROM salaries LIMIT 10`
-
+// noinspection JSUnusedGlobalSymbols
 export function setup() {
-    // 2. setup code
-    // The setup function runs, setting up the test environment and generating data. Optional.
-    // Purpose
-    // Set up data for processing, share data among VUs (Call API to start test environment; Once)
-
+    // 2. Setup code (call once at the beginning of test)
+    // Set up data for processing, share data among VUs.
+    console.log("setup is invoked.")
 }
 
-// Function to log and reset the trend metrics
-function logAndResetTrend() {
-    console.log(`p(90): ${queryDurationTrend.percentile(90)}`);
-    console.log(`p(95): ${queryDurationTrend.percentile(95)}`);
-    console.log(`p(99): ${queryDurationTrend.percentile(99)}`);
-    // Reset the trend metric by creating a new instance
-    //return new Trend("query_duration", true);
-}
-
-// setInterval(logAndResetTrend, 3000);
-
+// noinspection JSUnusedGlobalSymbols
 export default function () {
     // 3. VU code
-    // VU code runs in the default or scenario function, running for as long and as many times as the options define. Required.
-    // Purpose
-    // Run the test function, usually default (Make https requests, validate responses;Once per iteration, as many times as the test options require)
 
+    queryCount.add(1);
     const startTime = new Date();
 
     try {
-        //sql.query(db, query);
-        sql.query(db, query2);
-        queryCounter.add(1);
+        // noinspection SqlDialectInspection,SqlNoDataSourceInspection
+        const results = sql.query(db,
+            `SELECT *
+             FROM salaries LIMIT 10`
+        );
+        const checkOutput = check(results, {
+            'is length 10': (r: any[]) => r.length === 10,
+        })
+        if (!checkOutput) {
+            queryFailures.add(1)
+            console.warn("Results of sql.query() are invalid.")
+        }
     } catch (e) {
-        console.log(e)
-        failedQueryCounter.add(1);
+        // It might get exceptions due to too many connections or unexpected EOF.
+        // Please do not increase virtual users (VU) above high loads
+        // where it returns exceptions.
+        // Exception 이 발생하는 높은 부하 수준으로 Virtual User 를 늘이는 것을 권장하지 않습니다.
+        queryFailures.add(1)
+        console.warn("Got an exception:", e)
     }
 
     const endTime = new Date();
-    let duration = endTime - startTime;
-    queryDurationTrend.add(duration);
-    console.log("duration=" + duration);
-    sleep(0.3);
+    const duration = endTime.valueOf() - startTime.valueOf();
+    queryDuration.add(duration);
+
+    sleep(0.3); // in seconds
 }
 
+// noinspection JSUnusedGlobalSymbols
 export function teardown() {
-    // 4. teardown code
-    // The teardown function runs, postprocessing data and closing the test environment. Optional.
-    // Purpose
-    // Process result of setup code, stop test environment (Validate that setup had a certain result, send webhook notifying that test has finished; Once **)
-    // ** If the Setup function ends abnormally (e.g throws an error), the teardown() function isn't called. Consider adding logic to the setup() function to handle errors and ensure proper cleanup.
+    // 4. Teardown
+    // Process result of setup code, stop test environment.
     db.close();
 }
